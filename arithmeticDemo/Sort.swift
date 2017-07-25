1d15b2c8b249cf16818b287b6479e76d135bc835
wzxfaker <admin@faker.local>
2017-07-25 17:33:40 +0800

    会议时间
    会议时间算法题提交

--- arithmeticDemo/Sort.swift
+++ arithmeticDemo/Sort.swift
@@ -56,4 +56,50 @@ class Sort: NSObject {
 }
 
 
+/*
+ 已知有很多会议，如果有这些会议时间有重叠，则将它们合并。
+ 比如有一个会议的时间为3点到5点，另一个会议时间为4点到6点，那么合并之后的会议时间为3点到6点
+ 解题思路：解决算法题目第一步永远是把具体问题抽象化。这里每一个会议我们已知开始时间和结束时间，就可以写一个类来定义它，然后题目说已知有很多会议，就是说我们已知有一个MeetingTime的数组、所以题目就转化为写一个函数，输入为一个MeetingTime的数组，输出为一个将原数组中所有重叠时间都处理过的新数组。
+ */
+public class meetingTime {
+    public var start : Int;
+    public var end : Int;
+    public init(_ start : Int,_ end : Int) {
+        self.start = start;
+        self.end = end;
+    }
+}
+
+func merge(meetingTimes : [meetingTime]) -> [meetingTime]{
+    guard meetingTimes.count > 1 else {
+        return meetingTimes;
+    }
+    //排序
+    var meetingTimes = meetingTimes.sorted(){
+        if $0.start != $1.start {
+            return $0.start < $1.start;
+        }else{
+            return $0.end < $1.end;
+        }
+    }
+    
+    //新建结果数组
+    var resultArr = [meetingTime]();
+    resultArr.append(meetingTimes[0]);
+    
+    //遍历排序后的原数组，并与结果数组归并
+    for i in 1...meetingTimes.count-1 {
+        let last = resultArr[resultArr.count-1];
+        let current = meetingTimes[i];
+        if last.end < current.start {
+            resultArr.append(current);
+        }else{
+            last.end = max(last.end, current.end);
+        }
+    }
+    return resultArr;
+}
+
+
+
 
