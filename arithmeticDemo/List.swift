4200609b5da7450ddf5b2c46dd8111ad64b76690
wzxfaker <admin@faker.local>
2017-07-25 09:48:48 +0800

    链表处理
    链表处理

--- arithmeticDemo/List.swift
+++ arithmeticDemo/List.swift
@@ -15,7 +15,8 @@ enum ListEnum<Element>{
 
 class List: NSObject {
     //用枚举创建链表
-    
+//    let l = ListEnum<Int>.Empty.insert(x: 2).insert(x: 3).insert(x: 4).insert(x: 5);
+//    l.description();
 }
 
 extension ListEnum {
@@ -45,3 +46,123 @@ extension ListEnum {
         }
     }
 }
+
+//✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨链表✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨
+class ListNode {//结点
+    var val : Int;
+    var next : ListNode?
+    init(_ val : Int) {
+        self.val = val;
+        self.next = nil;
+    }
+}
+
+class ListX{//链表
+    var head : ListNode?;
+    var tail : ListNode?;
+    
+    //尾插法
+    func appendToTail(_ val : Int) {
+        if tail == nil {
+            tail = ListNode.init(val);
+            head = tail;
+        }else{
+            tail!.next = ListNode(val);
+            tail = tail!.next;
+        }
+    }
+    
+    //头插法
+    func appendToHead(_ val : Int) {
+        if head == nil {
+            head = ListNode(val);
+            tail = head;
+        }else{
+            let temp = ListNode(val);
+            temp.next = head;
+            head = temp;
+        }
+    }
+}
+
+//一道算法题：给一个链表和一个值x，要求将链表中所有小于x的值放到左边，所有大于等于x的值放到右边。原链表的节点顺序不能变。
+//例：1->5->3->2->4->2，给定x = 3。则我们要返回 1->2->2->3->5->4
+
+//思路：我们只要采用尾插法，遍历链表，将小于x值的节点接入新的链表即可。代码如下：
+func getLeftList(_ head : ListNode?,x : Int) -> ListNode?{
+    let dummy = ListNode(0);
+    var pre = dummy;
+    var node = head;
+    while node != nil {
+        if  node!.val < x {
+            pre.next = node;
+            pre = node!;
+        }
+        node = node!.next;
+    }
+    return dummy.next;
+}
+
+//算法题全部代码
+func partition(_ head : ListNode?,x : Int) -> ListNode?{
+    let prevDummy = ListNode(0);
+    var prev = prevDummy;
+    let postDummy = ListNode(0);
+    var post = postDummy;
+    
+    var node = head;
+    //尾插法处理左边和右边
+    while node != nil {
+        if node!.val < x {
+            prev.next = node;
+            prev = node!;//此处node是optional类型，而prev不是optional类型，所以需要用!转换一下
+        }else{
+            post.next = node;
+            post = node!;
+        }
+        node = node!.next;
+    }
+    //拼接左边和右边
+    post.next = nil;//防止链表循环指向构成环
+    prev.next = postDummy.next;//此处post和postDummy有什么区别
+    return prevDummy.next;
+}
+
+//快行指针，可以用来判断一个链表中是否有环
+func hasCycle(_ head : ListNode?) -> Bool{
+    var slow = head;
+    var fast = head;
+    while fast != nil && fast!.next!.next != nil   {
+        slow = slow!.next;
+        fast = fast!.next!.next;
+        if slow === fast {//==只能用来判断两个value是否相等
+            return true;
+        }
+    }
+    return false;
+}
+
+//算法：删除链表中倒数第n个节点。例：1->2->3->4->5，n = 2。返回1->2->3->5。
+//注意：给定n的长度小于等于链表的长度。
+func removeNthFromEnd(_ head : ListNode?,_ n : Int) -> ListNode?{
+    let dummy = ListNode(0);
+    dummy.next = head;
+    var prev : ListNode? = dummy;
+    var post : ListNode? = dummy;
+    //设置后一个结点的起始位置
+    for _ in 0..<n {
+        if post == nil {
+            break;
+        }
+        post = post!.next;
+    }
+    //同时移动两个结点
+    while post != nil && post!.next != nil {
+        prev = prev!.next;
+        post = post!.next;
+    }
+    //删除结点
+    prev!.next = prev!.next!.next;
+    return dummy.next;
+}
+
--- arithmeticDemo/Sort.swift
+++ arithmeticDemo/Sort.swift
@@ -10,25 +10,48 @@ import UIKit
 
 //排序方法的时间复杂度和空间复杂度参见sort.png
 class Sort: NSObject {
-    //归并排序
+    //归并排序：归并算法通常用递归实现，先把待排序区间[s,t]以中点二分，接着把左边子区间排序，再把右边子区间排序，最后把左区间和右区间用一次归并操作合并成有序的区间[s,t]
     func mergeSort(array : [Int]) -> [Int] {
         var helper = Array(repeating:0, count : array.count);//创建一个所有元素为0的数组
         var array = array;
-        mergeSort(&array, &helper, 0, array.count-1);
+        mergeSort1(&array, &helper, 0, array.count-1);
         return array;
     }
     
-    func mergeSort(inout array : [Int],inout _ helper : [Int],_ low : Int,_ high : Int) {//闭包
-        guard low > high else {
+    func mergeSort1(_ array : inout [Int],_ helper : inout [Int],_ low : Int,_ high : Int) {//闭包
+        guard low < high else {
             return;
         }
         let middle = (high - low) / 2 + low;
-        mergeSort(&array, &helper, low, high);
-        
+        mergeSort1(&array, &helper, low, middle);
+        mergeSort1(&array, &helper, middle+1, high);
+        merge(array: &array , helper: &helper, low, middle, high);
     }
     
-    
-    
+    func merge(array : inout [Int],helper : inout [Int],_ low : Int,_ middle : Int,_ high : Int) {
+        for i in low...high {
+            helper[i] = array[i];
+        }
+        var helperLeft = low;
+        var helperRight = middle+1;
+        var current = low;
+        while helperLeft <= middle && helperRight <= high {
+            if helper[helperLeft] <= helper[helperRight] {
+                array[current] = helper[helperLeft];
+                helperLeft += 1;
+            }else{
+                array[current] = helper[helperRight];
+                helperRight += 1;
+            }
+            current += 1;
+        }
+        guard middle - helperLeft >= 0 else {
+            return;
+        }
+        for i in 0...middle-helperLeft {
+            array[current+i] = helper[helperLeft+i];
+        }
+    }
     
 }
 
